2dba051d6526859c3dcbcefbe1f8a0ea46296c28
Ed Summers <ehs@pobox.com>
2019-12-11 08:32:05 -0500

    fixed module name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     author="Ed Summers",
     author_email="ehs@pobox.com",
-    packages=['filetest'],
+    packages=['datatest'],
     description="Example of bundling data with a python package",
     package_data={"datatest": ["buildings.csv"]},
 )
